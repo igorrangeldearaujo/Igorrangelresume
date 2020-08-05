bef3d85550ca5664caf0b940b0759d20d2e08b28
Igor <Igor44956@gmail.com>
2020-08-05 13:12:43 -0300

    atualizado

--- components/Summary.js
+++ components/Summary.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Summary = () => {
     return (
-        <div className='bg-white rounded-lg shadow-lg py-12 px-8 mx-8 md:mx-0 md:px-16'>
+        <div className='bg-white rounded-lg shadow-lg md:py-10 py-12 px-8 mx-8 md:mx-0 md:px-4'>
             <h3 className='text-4xl text-center text-orange'>About</h3>
-            <p className='text-2x1'>Teaching experience during the period from 2015 to 2019 proven in the Territorial Center for Professional Education of the State of Bahia in the Technical Course in Computer Science and in the function of Internship Teacher, In addition, act in the disciplines Life Project and World of Work in other courses (Administration, Agriculture, Logistics), as well as in the Federal Government Program (MEDIOTEC) in the function of teacher of technical disciplines of the Technical Course in Computer Networks.
+            <p className='text-2x1 text-justify'>Teaching experience during the period from 2015 to 2019 proven in the Territorial Center for Professional Education of the State of Bahia in the Technical Course in Computer Science and in the function of Internship Teacher, In addition, act in the disciplines Life Project and World of Work in other courses (Administration, Agriculture, Logistics), as well as in the Federal Government Program (MEDIOTEC) in the function of teacher of technical disciplines of the Technical Course in Computer Networks.
           During my time as a teacher, I participated and carried out the growing and undertaking training offered by SEBRAE in partnership with the state government. </p>
         </div>
 
